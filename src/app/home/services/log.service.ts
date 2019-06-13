6d996710608573c4c4fccea4feb422a2a29fef7f
ian klek <ian.klek@gmail.com>
2019-06-13 20:21:56 +0100

    Extra log functionality

--- src/app/home/services/log.service.ts
+++ src/app/home/services/log.service.ts
@@ -1,20 +1,32 @@
 import {Injectable} from "@angular/core";
+import {Platform} from "@ionic/angular";
+import {Storage} from "@ionic/storage";
 import {BehaviorSubject} from "rxjs";
 
 
 @Injectable({providedIn: "root"})
 export class LogService  {
 
+    constructor(
+        private storage: Storage,
+        private platform: Platform) {
+}
+
     public Messages: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
     log(msg: string, obj: any = null) {
+        const date = new Date().toDateString();
         if (obj) {
             this.Messages.next(
-                [msg + ": " + JSON.stringify(obj)].concat(this.Messages
+                [date + " " + msg + ": " + JSON.stringify(obj)].concat(this.Messages
                     .getValue()));
+            this.platform.ready().then(async () => {
+                await this.storage.ready();
+                await this.storage.set("ecotrack-logs", this.Messages.getValue());
+            });
         } else {
             this.Messages.next(
-                [msg].concat(this.Messages
+                [date + " " + msg].concat(this.Messages
                     .getValue()));
         }
     }
